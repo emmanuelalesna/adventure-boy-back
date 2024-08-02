4f11cf8ed7009681d29d62245a2e6b0191c7d061
emmanuelalesna <emmanuelalesna@gmail.com>
2024-08-02 15:26:03 -0400

    added CORS and dependency injection for repos

--- Project2.app/Program.cs
+++ Project2.app/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using Project2.app.DataAccess;
+using Project2.app.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,8 +11,23 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(co => {
+    co.AddPolicy("CORS" , pb =>{
+        pb.WithOrigins("*")
+        .AllowAnyHeader();
+    });
+});
+
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IRepo<Account>, AccountRepo>();
+builder.Services.AddScoped<IRepo<Enemy>, EnemyRepo>();
+builder.Services.AddScoped<IRepo<Item>, ItemRepo>();
+builder.Services.AddScoped<IRepo<Player>, PlayerRepo>();
+builder.Services.AddScoped<IRepo<Room>, RoomRepo>();
+builder.Services.AddScoped<IRepo<Shop>, ShopRepo>();
+builder.Services.AddScoped<IRepo<Spell>, SpellRepo>();
 
 var app = builder.Build();
 // Configure the HTTP request pipeline.
@@ -20,6 +36,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+app.UseCors("CORS");
 
 app.UseHttpsRedirection();
 
