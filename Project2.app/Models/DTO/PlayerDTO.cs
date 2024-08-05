9f28c1765df9d3ea5621244e1ac8d3b3f6fb70da
emmanuelalesna <emmanuelalesna@gmail.com>
2024-08-05 16:12:04 -0400

    dto

--- Project2.app/Models/DTO/PlayerDTO.cs
+++ Project2.app/Models/DTO/PlayerDTO.cs
@@ -3,10 +3,5 @@ namespace Project2.app.Models.DTO;
 
 public class PlayerDTO
 {
-    public int Health { get; set; } = 100;
-    public int Level { get; set; } = 1;
-    public int Exp { get; set; } = 0;
-    public int Mana { get; set; } = 0;
-    public int Gold { get; set; } = 0;
-    public Room CurrentRoom { get; set; }
+    public required string Name { get; set; }
 }
