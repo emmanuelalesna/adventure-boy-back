b17a7143042ec18859f233f9d6abc1e485ae24f8
Ian <ianbraverman@hotmail.com>
2024-08-08 14:53:51 -0400

    added room 2 spells

--- Project2.app/DataAccess/ApplicationDBContext.cs
+++ Project2.app/DataAccess/ApplicationDBContext.cs
@@ -24,6 +24,8 @@ public class ApplicationDbContext : DbContext
         .WithOne(a => a.OwnedPlayer)
         .HasForeignKey<Player>(p => p.PlayerId);
 
+
+        //room 1 information
         modelBuilder.Entity<Enemy>().HasData(new Enemy { EnemyId = 1, EnemyName = "Angry Wolf", Attack = 1, Health = 3, ImageUrl = "https://api.scryfall.com/cards/named?fuzzy=end-the-festivities" });
 
         modelBuilder.Entity<Room>().HasData(new Room { RoomId = 1, ImageUrl = "https://api.scryfall.com/cards/named?fuzzy=bayou" });
@@ -31,7 +33,19 @@ public class ApplicationDbContext : DbContext
         modelBuilder.Entity<Item>().HasData(new Item { ItemId = 1, ItemName = "Bronze Sword", Attack = 1, ImageUrl = "https://api.scryfall.com/cards/named?fuzzy=bronze-sword" });
 
         modelBuilder.Entity<Spell>().HasData(new Spell { SpellId = 1, SpellName = "Lightning Bolt", Attack = 2, ManaCost = 1, ImageUrl = "https://api.scryfall.com/cards/named?fuzzy=lightning-bolt" });
+
+        //room 2 information
+
+        modelBuilder.Entity<Enemy>().HasData(new Enemy { EnemyId = 2, EnemyName = "Evil Skeleton", Attack = 2, Health = 5, ImageUrl = "https://api.scryfall.com/cards/named?fuzzy=manor-skeleton" });
+
+        modelBuilder.Entity<Room>().HasData(new Room { RoomId = 2, ImageUrl = "https://api.scryfall.com/cards/named?fuzzy=throne-of-the-high-city" });
+
+        modelBuilder.Entity<Item>().HasData(new Item { ItemId = 2, ItemName = "Iron Sword", Attack = 2, ImageUrl = "https://api.scryfall.com/cards/named?fuzzy=greatsword" });
+
+        modelBuilder.Entity<Spell>().HasData(new Spell { SpellId = 2, SpellName = "Fire Blast", Attack = 3, ManaCost = 2, ImageUrl = "https://api.scryfall.com/cards/named?fuzzy=fireblast" });
     }
+
+
 
 
 }
