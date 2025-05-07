2b310ee560c11b4d9d63e23c70faa3fa22740cf6
emmanuelalesna <emmanuelalesna@gmail.com>
2025-05-07 17:22:49 -0400

    readding

--- Project2.app/Models/Account.cs
+++ Project2.app/Models/Account.cs
@@ -6,7 +6,7 @@ public class Account
 {
     [Key]
     public int AccountId { get; set; }
-    public string Username { get; set; }
-    public string Password { get; set; }
+    public required string Username { get; set; }
+    public required string Password { get; set; }
     public Player? OwnedPlayer { get; set; }
 }
